0fabe873b3735af95d0d3ce1b0956381e55077b7
Akmal Oriqov <akmalbekortiqov7976@gmail.com>
2022-12-20 23:35:25 +0500

    ts masalalar beginer 18-22

---
+++ 1dars/hello.ts
@@ -0,0 +1,58 @@
+// masala 18
+// let a:number=10
+// let c:number=20
+// let b:number=30
+// let d:number=a*c
+// let t:number=b*c
+// console.log(`${d*t}`);
+
+// // masala 19
+// let a1: number = 1;
+// let b1: number = 2;
+// let x1: number = 3;
+// let y1: number = 5;
+// let x2: number = 7;
+// let y2: number = 6;
+// let a: number;
+// let b: number;
+// let s: number;
+// let p: number;
+
+// a = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+// b = Math.sqrt(Math.pow(x1-a1, 2) + Math.pow(y1-b1, 2));
+
+// console.log(`Yuza: ${a * b} Perimetr: ${2 * (a + b)}`);
+
+
+// masala 20
+// let x1:number=10
+// let y1:number=15
+// let x2:number=20
+// let y2:number=25
+// let a:number= x2-x1
+// let b:number= y2-y2
+// let d:number= a*a
+// let c:number= b*b
+// console.log(`${Math.sqrt(d+c)}`);
+
+// masala 21 
+// let x1:number=10
+// let x2:number=20
+// let x3:number=30
+// let y1:number=15
+// let y2:number=25
+// let y3:number=35
+// let a:number=y1-x1
+// let b:number=y2-x2
+// let c:number=y3-x3
+// let p:number=(a+b+c)/2
+// let q:number=p-a
+// let w:number=p-b
+// let e:number=p-c
+// console.log(`S=${Math.sqrt(p*q*w*e)}`);
+ 
+// masala 22
+// let a:number=10
+// let b:number=20
+// let d:number=15
+
