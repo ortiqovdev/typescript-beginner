b702d1f46d6824f1d579906fe9b2b9e762ec71cf
Akmal Oriqov <akmalbekortiqov7976@gmail.com>
2022-12-21 21:20:46 +0500

    Integer 11-23 +prompt

---
+++ 2dars/prompt.js
@@ -0,0 +1,91 @@
+
+let prompt = require('prompt-sync')()
+
+// Integer11
+// let a =prompt('3 xonali son kiriting : ')
+// let b=Math.floor(a/100)
+// let c=Math.ceil(a%10)
+// let d=Math.ceil(a%100)
+// let q=Math.floor(d/10)
+// console.log(b+c+q);
+
+// Integer12
+// let a =prompt('3 xonali son kiriting : ')
+// let b=Math.floor(a%10)
+// let c=Math.floor(a/100)
+// let d=Math.floor(a/10)
+// let h=Math.ceil(d%10)
+// console.log(`${b}${h}${c}`);
+
+// Integer13
+// let a =prompt('3 xonali son kiriting : ')
+// let b=Math.floor(a%10)
+// let c=Math.floor(a/100)
+// let d=Math.floor(a/10)
+// let h=Math.ceil(d%10)
+// console.log(`${h}${b}${c}`);
+
+// Integer14
+// let a =prompt('3 xonali son kiriting : ')
+// let b=Math.floor(a%10)
+// let c=Math.floor(a/100)
+// let d=Math.floor(a/10)
+// let h=Math.ceil(d%10)
+// console.log(`${b}${c}${h}`);
+
+// Integer15
+// let a=123
+// let b=Math.floor(a%10)
+// let c=Math.floor(a/100)
+// let d=Math.floor(a/10)
+// let h=Math.ceil(d%10)
+// console.log(`${h}${c}${b}`);
+
+// Integer16
+// let a=123
+// let b=Math.floor(a%10)
+// let c=Math.floor(a/100)
+// let d=Math.floor(a/10)
+// let h=Math.ceil(d%10)
+// console.log(`${c}${b}${h}`);
+
+// Integer17
+// let a =prompt('4 xonali son kiriting : ')
+// let b=Math.floor(a%1000)
+// let c=Math.floor(b/100)
+// console.log(c);
+
+// Integer18
+// let a =prompt('4 xonali son kiriting : ')
+// let b=Math.floor(a/100)
+// let c=Math.floor(b/10)
+// console.log(c);
+
+// Integer19
+// let a =prompt('Sekundlarni kiriting : ')
+// let b=Math.floor(a/60)
+// console.log(`${a} sekund to'liq  ${b} daqiqa bo'ladi`);
+
+// Integer20
+// let a =prompt('Sekundlarni kiriting : ')
+// let b=Math.floor(a/3600)
+// console.log(`${a} sekund to'liq  ${b} soat bo'ladi`);
+
+// Integer21
+// let a =prompt('Sekundlarni kiriting : ')
+// let m=Math.floor(a/60)
+// let s=Math.floor(a%60)
+// console.log(`${a} sekundda ${m} daqiqa va ${s} sekund bor`);
+
+// Integer22
+// let a =prompt('Sekundlarni kiriting : ')
+// let b=Math.floor(a/3600)
+// let c=Math.floor(a%3600)
+// console.log(`${a} sekundda to'liq ${b} soat va ${c} sekund bor`);
+
+// Integer23
+// let a =prompt('Sekundlarni kiriting : ')
+// let b=Math.floor(a/3600)
+// let d=Math.floor((a%3600)/60)
+// let c=Math.floor((a%3600)%60)
+// console.log(`${a} sekunda ${b} soat , ${d} minut va ${c}sekunt bor`);
